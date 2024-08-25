882c33223fab4b04695fcdaae958935fa62505c3
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-08-26 02:27:15 +0900

    Init

--- 1_array_hashing/group_anagram.py
+++ 1_array_hashing/group_anagram.py
@@ -38,7 +38,7 @@ from typing import List
 
 class Solution:
     def dictionary(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        res: dict = defaultdict(list)
         for s in strs:
             count = [0] * 26
             for c in s:
